0319e921cd5235449081e99a07d920a8a220d6c5
aungkaungkhant-nssh <akkgit0909@gmail.com>
2025-06-17 20:22:44 +0630

    login ui design

--- navigator/AuthNavigator.tsx
+++ navigator/AuthNavigator.tsx
@@ -4,7 +4,21 @@ import React from 'react'
 const AuthNavigator = () => {
     return (
         <Stack>
-            <Stack.Screen name="(auth)"></Stack.Screen>
+            <Stack.Screen
+                name="(auth)"
+                options={{
+                    title: 'ယာဉ်စည်းကမ်းလမ်းစည်းကမ်း ထိန်သိမ်းရေးကော်မတီ',
+                    headerStyle: {
+                        backgroundColor: '#000080',  // Navy blue color or any color you want
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontSize: 15,  // smaller font size
+                    },
+                    headerTitleAlign: 'center',
+                }}
+
+            />
         </Stack>
     )
 }
